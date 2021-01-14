2c5c9e79e80efcd4cee2c2321b52d4ed740ed834
Yeraldina Estrella <yestrella14@gmail.com>
2021-01-13 23:32:50 -0500

    Changes

---
+++ helper_functions.py
@@ -0,0 +1,8 @@
+#Check a dataframe for nulls and return the number of missing values
+def null_count(df)
+
+
+
+#Create a Train/Test split function for a dataframe and returns both the Training and Testing sets. 
+#Frac referes to the precent of data you would like to set aside for training.
+def train_test_split(df, frac): 
